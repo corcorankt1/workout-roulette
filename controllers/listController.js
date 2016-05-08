4535f728f992cc3960af898e42956d9246417764
Katie Corcoran <corcorankt1@gmail.com>
2016-05-08 14:31:04 -0400

    add service and logic to set array to id on index.html

--- controllers/listController.js
+++ controllers/listController.js
@@ -1,7 +1,8 @@
 var app = angular.module("workout");
 
-app.controller("listController", function($scope, listService){
+app.controller("listController", function($scope, listService, idService){
 	var exercise = listService.sendData();
+	var id = idService.sendData();
 	var biceps = [];
 	var shoulders = [];
 	var core = [];
@@ -11,49 +12,104 @@ app.controller("listController", function($scope, listService){
 	var glutes = [];
 	var back = [];
 	var quads = [];
+	var array = [];
 	exercise.forEach(function(elem){
 		elem.muscles.forEach(function(el){
-			if(el===1 || el===11 || el===13){
-				biceps.push({name: elem.name, description: elem.description});
-			} else if(el===2){
-				shoulders.push({name: elem.name, description: elem.description});
-			} else if(el===3 || el===6 || elem===14){
-				core.push({name: elem.name, description: elem.description});
-			} else if(el===4){
-				chest.push({name: elem.name, description: elem.description});
-			} else if(el===5){
-				triceps.push({name: elem.name, description: elem.description});
-			} else if(el===7 || el===15){
-				calves.push({name: elem.name, description: elem.description});
-			} else if(el===8){
-				glutes.push({name: elem.name, description: elem.description});
-			} else if(el===9 || el==12){
-				back.push({name: elem.name, description: elem.description});
-			} else if(el===10){
-				quads.push({name: elem.name, description: elem.description});
-			} else {
-				return false;
+			switch(el){
+				case 1:
+				case 11:
+				case 13:
+					biceps.push({name: elem.name, description: elem.description})
+					break;
+				case 2:
+					shoulders.push({name: elem.name, description: elem.description})
+					break;
+				case 3:
+				case 6:
+				case 14:
+					core.push({name: elem.name, description: elem.description})
+					break;
+				case 4:
+					chest.push({name: elem.name, description: elem.description})
+					break;
+				case 5:
+					triceps.push({name: elem.name, description: elem.description})
+					break;
+				case 7:
+				case 15:
+					calves.push({name: elem.name, description: elem.description})
+					break;
+				case 8:
+					glutes.push({name: elem.name, description: elem.description})
+					break;
+				case 9:
+				case 12:
+					biceps.push({name: elem.name, description: elem.description})
+					break;
+				case 10:
+					quads.push({name: elem.name, description: elem.description})
+					break;
+				default:
+					return exercise;
 			}
 		});
 	});
-
+	switch(id){
+		case "bisLeft":
+		case "bisRight":
+			array = biceps
+			break;
+		case "chest":
+			array = chest
+			break;
+		case "abs":
+			array = core
+			break;
+		case "quadLeft":
+		case "quadRight":
+			array = quads
+			break;
+		case "shoulderLeft":
+		case "shoulerRight":
+			array = shoulders
+			break;
+		case "upper":
+			array = back
+			break;
+		case "leftGlute":
+		case "rightGlute":
+		case "thighRight":
+		case "thighLeft":
+			array = glutes
+			break;
+		case "trisLeft":
+		case "trisRight":
+			array = triceps
+			break;
+		case "calfLeft":
+		case "calfRight":
+			array = calves
+			break;
+		default:
+			array = exercise
+	}
+	console.log(array);
 	var arr = []
 	while(arr.length < 8){
-  	var randomnumber=Math.ceil(Math.random()*biceps.length)
+  	var randomnumber=Math.ceil(Math.random()*array.length)
   	var found=false;
   	for(var i=0;i<arr.length;i++){
 	if(arr[i]==randomnumber){found=true;break}
   	}
   	if(!found)arr[arr.length]=randomnumber;
 	}
-	console.log(arr);
-	$scope.exercise1 = biceps[arr[0]].name;
-	$scope.exercise2 = biceps[arr[1]].name;
-	$scope.exercise3 = biceps[arr[2]].name;
-	$scope.exercise4 = biceps[arr[3]].name;
-	$scope.exercise5 = biceps[arr[4]].name;
-	$scope.exercise6 = biceps[arr[5]].name;
-	$scope.exercise7 = biceps[arr[6]].name;
-	$scope.exercise8 = biceps[arr[7]].name;
+	$scope.exercise1 = array[arr[0]].name;
+	$scope.exercise2 = array[arr[1]].name;
+	$scope.exercise3 = array[arr[2]].name;
+	$scope.exercise4 = array[arr[3]].name;
+	$scope.exercise5 = array[arr[4]].name;
+	$scope.exercise6 = array[arr[5]].name;
+	$scope.exercise7 = array[arr[6]].name;
+	$scope.exercise8 = array[arr[7]].name;
 
 });
